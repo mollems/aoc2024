33c49c56f1102fac23b5c4f341d04a138339701d
M. Sean Molley <sean@basementsoftware.com>
2026-09-06 12:46:50 -0400

    Make helloworld.go format the date and time

--- helloworld.go
+++ helloworld.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
-	fmt.Println("Hello, world!")
+	currentTime := time.Now()
+	formattedTime := currentTime.Format("2006-01-02 15:04:05 PM MST")
+
+	fmt.Println("Hello, world, at:", formattedTime)
 }
